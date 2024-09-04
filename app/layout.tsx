4ec8bba4d00bf113a5c9b2cd353f16a4e4524348
Maya Eid <mayabaseleid@gmail.com>
2024-09-04 15:41:59 +0300

    Add next-themes dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Providers from "@/components/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return ( //passing the nested pages and layouts here to the body
-    <html lang="en">
-      <body className={inter.className}>{children}</body> 
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+        </body> 
     </html>
   );
 }
