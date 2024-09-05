1e30735865634f0f336d34ea339afa185fbd9ea1
Maya Eid <mayabaseleid@gmail.com>
2024-09-05 23:24:59 +0300

    Create custom code component

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -3,6 +3,15 @@ import { highlight } from 'sugar-high'
 import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
 import Counter from '@/components/counter'
     
+function Code({ children, ...props }: any) { 
+    let codeTML = highlight(children) 
+    return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} /> 
+} 
+
+const components = { 
+    code: Code, Counter 
+}
+
 export default function MDXContent(
     props: JSX.IntrinsicAttributes & MDXRemoteProps
 ){
