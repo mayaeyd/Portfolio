48143bcfeba1aa02b4f831c8fbdcba107a81b0ce
Maya Eid <mayabaseleid@gmail.com>
2024-09-05 15:33:21 +0300

    Set sans and serif fonts in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
+import {fontFamily} from 'tailwindcss/defaultTheme';
 import type { Config } from "tailwindcss";
+
 
 const config: Config = {
     darkMode: ["class"],
@@ -7,6 +9,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix:'',
   theme: {
 	container :{      //when shadcn is installed it sets this container class in the tailwind config  
 		center:true,
@@ -16,6 +19,10 @@ const config: Config = {
 		},
 	},
   	extend: {
+		fontFamily:{
+			sans: ['var(--font-sans)', ...fontFamily.sans],
+			serif: ['var(--font-serif)', ...fontFamily.serif]
+		},
   		backgroundImage: {
   			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
   			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
