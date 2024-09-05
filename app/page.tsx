6ed1324491d1bd8075c53f98efebfd78f039eb06
Maya Eid <mayabaseleid@gmail.com>
2024-09-05 16:29:57 +0300

    Add intro component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 //this is the homepage. Anytime we want to craete a route index.js, we have to export a page.tsx or jsx from that directory 
+import Intro from "@/components/intro";
 import React from "react";
 
 export default function Home(){
   return(
     <section className="py-24">
       <div className="container max-w-3xl">
-        <h1 className="text-3xl font-bold">
-          Portfolio
-        </h1>
+        <Intro />
       </div>
     </section>
   )
