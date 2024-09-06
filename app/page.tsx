1c61cdbcf95e92176b71a4c2bf71847ef80f3865
Maya Eid <mayabaseleid@gmail.com>
2024-09-06 20:35:36 +0300

    Add recent posts to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 //this is the homepage. Anytime I want to create a route index.js, I have to export a page.tsx or jsx from that directory 
 import Intro from "@/components/intro";
-import {MDXRemote} from 'next-mdx-remote/rsc';
+import RecentPosts from "@/components/recent-posts";
 
 export default function Home(){
-  const content = '# This is a heading'
   return(
     <section className="py-24">
       <div className="container max-w-3xl">
         <Intro />
-        <MDXRemote source={content} />
+        <RecentPosts />
       </div>
     </section>
   )
