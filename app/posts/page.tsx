14d002df5168b440bf036a9043d892f0d839f82e
Maya Eid <mayabaseleid@gmail.com>
2024-09-06 16:37:24 +0300

    Initialize all posts page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+//all posts page
+import {getPosts} from '@/lib/posts'
+import PostsWithSearch from '@/components/posts-with-search'
 
-export default function PostsPage() {
-  return (
-    <div>
-      PostsPage
-    </div>
+export default async function PostsPage(){
+  const posts = await getPosts()
+
+  return(
+    <section className="pb-24 pt-40">
+      <div className="container max-w-3xl">
+        <h1 className="title mb-12">Posts</h1>
+
+        <Posts posts={posts} />
+      </div>
+    </section>
   )
 }
