3b210725e2e4651802ee42a43034d84ee6569b68
Maya Eid <mayabaseleid@gmail.com>
2024-09-06 17:09:07 +0300

    Create getPosts and getPostMetadata functions

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,6 +1,7 @@
 //all posts page
 import {getPosts} from '@/lib/posts'
-import PostsWithSearch from '@/components/posts-with-search'
+//import PostsWithSearch from '@/components/posts-with-search'
+import Posts from '@/components/posts'
 
 export default async function PostsPage(){
   const posts = await getPosts()
--- lib/posts.ts
+++ lib/posts.ts
@@ -30,3 +30,29 @@ export async function getPostBySlug(slug:string): Promise<Post | null> {  //retu
         return null
     }
 }
+
+export async function getPosts(limit?: number): Promise<PostMetadata[]> {
+    const files = fs.readdirSync(rootDirectory)
+    const posts = files
+        .map(file => getPostMetadata(file))  //get the metadata of each file
+        .sort((a, b) => {  //sort the posts according to published date
+            if (new Date(a.publishedAt ?? '') < new Date(b.publishedAt ?? '')) {
+                return 1
+            } else {
+                return -1
+            }
+        })
+    if (limit) {  //if a limit for num of posts was passed
+        return posts.slice(0, limit)
+    }
+    
+    return posts
+}
+
+export function getPostMetadata(filepath: string): PostMetadata {
+    const slug = filepath.replace(/\.mdx$/, '')  //get the file
+    const filePath= path.join(rootDirectory, filepath)  //create the slug
+    const fileContent= fs.readFileSync(filePath, {encoding: 'utf8'}) //read the content of the file
+    const {data} =matter(fileContent) //extract the front matter
+    return {...data,slug} //return the metadata
+}
